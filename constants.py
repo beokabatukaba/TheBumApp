d6ba1be9c9f13ea4b34e0c4dc58df45f9a19795f
john.m.walker <john.m.walker2@gmail.com>
2022-03-01 17:03:31 -0600

    many upgrades, including !fancy

---
+++ constants.py
@@ -0,0 +1,42 @@
+LIST_OF_COMMANDS = """
+TEXT COMMANDS
+-------------
+!YEE: Display a random snippet from our holy text.
+!YEEE: Display the inspiration for the whole YEEEEEEEEEEEEEEEEEE thing.
+!legend: Display the names of the actual humans(?) behind the masks.
+!roles: Display the primary scriptural inspiration for our roles.
+!wut: Display an explanation of this bot.
+!help/!commands/!list/!?: Display this very message.
+!SHEM: Display an excerpt about Shem.
+!SHEMM: Display a more problematic extension of the excerpt about Shem.
+
+GENERAL AUDIO COMMANDS
+----------------------
+!stop: Scold Christ-o-tron for being a bad bot (i.e. make it shut up)
+!volume: Provide an integer to set the volume. TBH I don't remember what the range is so God be with you.
+
+SPEECH COMMANDS
+---------------
+!YEEe: A random reading from our holy text.
+!YEEEe: Read the inspiration for the whole YEEEEEEEEEEEEEEEEEE thing.
+!SHEMe: Read an excerpt about Shem.
+!SHEMMe: Read the more problematic extension of the excerpt about Shem.
+
+MUSIC COMMANDS
+--------------
+!yt: Download and play a video from YT (please patiently wait for download).
+!stream: Stream a video from YT. More susceptible to network conditions, but better for longer videos.
+!play: Play a local file.
+!fancy: you know ;)
+"""
+
+MAX_NUMBER_OF_HOLY_LINES = 10
+ENOS_START_LINE_NUMBER   = 103
+ENOS_NUMBER_OF_LINES     = 13
+YEE_START_LINE_NUMBER    = 198
+YEE_NUMBER_OF_LINES      = 6
+SHEM_START_LINE_NUMBER   = 299
+SHEM_NUMBER_OF_LINES     = 10
+SHEM_PROBLEMATIC_NUMBER_OF_LINES = 13
+LOCALIZATIONS = "com ad ae com.af com.ag com.ai al am co.ao com.ar as at com.au az ba com.bd be bf bg com.bh bi bj com.bn com.bo com.br bs bt co.bw by com.bz ca cd cf cg ch ci co.ck cl cm cn com.co co.cr com.cu cv com.cy cz de dj dk dm com.do dz com.ec ee com.eg es com.et fi com.fj fm fr ga ge gg com.gh com.gi gl gm gr com.gt gy com.hk hn hr ht hu co.id ie co.il im co.in iq is it je com.jm jo co.jp co.ke com.kh ki kg co.kr com.kw kz la com.lb li lk co.ls lt lu lv com.ly co.ma md me mg mk ml com.mm mn ms com.mt mu mv mw com.mx com.my co.mz com.na com.ng com.ni ne nl no com.np nr nu co.nz com.om com.pa com.pe com.pg com.ph com.pk pl pn com.pr ps pt com.py com.qa ro ru rw com.sa com.sb sc se com.sg sh si sk com.sl sn so sm sr st com.sv td tg co.th com.tj tl tm tn to com.tr tt com.tw co.tz com.ua co.ug co.uk com.uy co.uz com.vc co.ve vg co.vi com.vn vu ws rs co.za co.zm co.zw cat"
+FANCY_YT_LINK = "https://www.youtube.com/watch?v=Pa6yt970yZo"
